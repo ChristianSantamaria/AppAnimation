ad4f4def7b13757823ff9a08eea9232c802c26fd
ChristianSantamaria <christianpopi@hotmail.com>
2018-03-14 16:25:05 +0100

    Creacion main2, layout y carpeta de menu

---
+++ app/src/main/java/com/example/christian/appanimation/Main2Activity.kt
@@ -0,0 +1,18 @@
+package com.example.christian.appanimation
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+    }
+}
